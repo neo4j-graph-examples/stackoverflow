f664075d47b60538372e845c903771e1a994176c
Florent Biville <445792+fbiville@users.noreply.github.com>
2020-11-02 14:56:31 +0100

    Minor improvements for code (#5)
    * Use var declaration to enforce no slice allocation
    * Favor const over var
    * Favor arrow function

--- code/go/example.go
+++ code/go/example.go
@@ -35,7 +35,7 @@ func runQuery(uri, username, password string) ([]string, error) {
 		if err != nil {
 			return nil, err
 		}
-		arr := make([]string, 0)
+		var arr []string
 		for result.Next() {
 			value, found := result.Record().Get("actorName")
 			if found {
--- code/javascript/example.js
+++ code/javascript/example.js
@@ -1,27 +1,27 @@
 // npm install --save neo4j-driver
 // node example.js
-var neo4j = require('neo4j-driver');
-var driver = neo4j.driver('neo4j+s://demo.neo4jlabs.com:7687', 
+const neo4j = require('neo4j-driver');
+const driver = neo4j.driver('neo4j+s://demo.neo4jlabs.com:7687',
                   neo4j.auth.basic('mUser', 's3cr3t'), 
                   {/* encrypted: 'ENCRYPTION_OFF' */});
 
-var query = 
+const query =
   `
   MATCH (m:Movie {title:$movieTitle})<-[:ACTED_IN]-(a:Person) RETURN a.name as actorName
   `;
 
-var params = {"movieTitle": "The Matrix"};
+const params = {"movieTitle": "The Matrix"};
 
-var session = driver.session({database:"movies"});
+const session = driver.session({database:"movies"});
 
 session.run(query, params)
-  .then(function(result) {
-    result.records.forEach(function(record) {
+  .then((result) => {
+    result.records.forEach((record) => {
         console.log(record.get('actorName'));
-    })
-	session.close();
+    });
+    session.close();
     driver.close();
   })
-  .catch(function(error) {
-    console.log(error);
+  .catch((error) => {
+    console.error(error);
   });
