b59dcbd671f7b146ddb9c8e6a6e8e51bf2463075
Florent Biville <445792+fbiville@users.noreply.github.com>
2021-01-14 15:07:19 +0100

    Fix Python formatting

--- code/python/example.py
+++ code/python/example.py
@@ -4,7 +4,7 @@
 from neo4j import GraphDatabase, basic_auth
 
 driver = GraphDatabase.driver(
-  "neo4j+s://demo.neo4jlabs.com:7687", 
+  "neo4j+s://demo.neo4jlabs.com:7687",
   auth=basic_auth("mUser", "s3cr3t"))
 
 cypher_query = '''
@@ -14,8 +14,7 @@ MATCH (m:Movie {title:$movieTitle})<-[:ACTED_IN]-(a:Person) RETURN a.name as act
 with driver.session(database="movies") as session:
   results = session.read_transaction(
     lambda tx: tx.run(cypher_query,
-      movieTitle="The Matrix").data())
-
+                      movieTitle="The Matrix").data())
   for record in results:
     print(record['actorName'])
 
