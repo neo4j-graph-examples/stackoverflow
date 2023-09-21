cd0f1b8473d773a24b83adc91724d2e121ec2d58
Sandbox 🤖 <neo4j-oss-build@neotechnology.com>
2023-09-21 11:27:59 +0000

    Triggered by direct commit. Origin: https://github.com/neo4j-contrib/sandbox-code-updater/commit/c25054b5806270c538866ae20a9097c3f6b30591

--- code/python/example.py
+++ code/python/example.py
@@ -1,22 +1,20 @@
-# pip3 install neo4j-driver
+# pip3 install neo4j
 # python3 example.py
 
 from neo4j import GraphDatabase, basic_auth
-
-driver = GraphDatabase.driver(
-  "bolt://<HOST>:<BOLTPORT>",
-  auth=basic_auth("<USERNAME>", "<PASSWORD>"))
 
 cypher_query = '''
 MATCH (t:Tag {name:$tagName})<-[:TAGGED]-(q:Question)<-[:ANSWERED]-(a:Answer {is_accepted:true})<-[:PROVIDED]-(u:User)
 RETURN u.display_name as answerer LIMIT 5
 '''
 
-with driver.session(database="neo4j") as session:
-  results = session.read_transaction(
-    lambda tx: tx.run(cypher_query,
-                      tagName="neo4j").data())
-  for record in results:
-    print(record['answerer'])
-
-driver.close()
+with GraphDatabase.driver(
+    "neo4j://<HOST>:<BOLTPORT>",
+    auth=("<USERNAME>", "<PASSWORD>")
+) as driver:
+    result = driver.execute_query(
+        cypher_query,
+        tagName="neo4j",
+        database_="neo4j")
+    for record in result.records:
+        print(record['answerer'])
